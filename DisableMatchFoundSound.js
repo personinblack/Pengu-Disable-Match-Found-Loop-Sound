c893611b89cec9a9a285a8118b88cebabac218b9
E. Berkay Öztürk <berkay@tuta.io>
2025-11-02 16:09:05 +0300

    Fix it again by hooking createSound instead of playSound
    Not every sound playback call playSound func anymore. Hopefully this won't break.

--- DisableMatchFoundSound.js
+++ DisableMatchFoundSound.js
@@ -2,36 +2,46 @@
  * @name DisableMatchFoundSound
  * @author menfie
  * @description Disables sounds "sfx-readycheck-ringmagic-accepted-loop" and "sfx-readycheck-sr-portal"
- * @version 0.0.7
+ * @version 0.0.7.1
  * @link https://github.com/personinblack/Pengu-Disable-Match-Found-Loop-Sound
  */
 
 export async function load() {
     const audioPlugin = await rcp.whenReady("rcp-fe-audio");
 
-    // Hooking all channels' playSound just in case.
-    const channelNames = [
-        "sfx",
-        "sfx-notifications",
-        "sfx-ui",
-        "sfx-champions",
+    const blockedUrls = [
+        "sfx-readycheck-ringmagic-accepted-loop", 
+        "sfx-readycheck-sr-portal"
     ];
-    
-    const blockedUrls = ["sfx-readycheck-ringmagic-accepted-loop", "sfx-readycheck-sr-portal"];
-    
+
+    const dummySound = {
+        play: () => Promise.resolve(null),
+        stop: () => Promise.resolve(null),
+        pause: () => Promise.resolve(null),
+        dispose: () => Promise.resolve(null),
+        setVolume: () => {},
+        getVolume: () => 0,
+        isPlaying: () => false,
+        on: () => {},
+        off: () => {},
+        trigger: () => {}
+    };
+
+    const channelNames = Object.values(audioPlugin.getChannelsNames());
+
     channelNames.forEach(channelName => {
         const channel = audioPlugin.getChannel(channelName);
-        if (channel && channel.playSound) {
-            const originalPlaySound = channel.playSound.bind(channel);
+        if (channel && channel.createSound) {
+            const originalCreateSound = channel.createSound.bind(channel);
             
-            channel.playSound = function(url, options, playOptions) {
+            channel.createSound = function(url, options) {
                 if (url && blockedUrls.some(blocked => url.includes(blocked))) {
                     console.log("BLOCKED AUDIO: ", url);
-                    return Promise.resolve(null);
+                    return dummySound;
                 }
                 
-                return originalPlaySound(url, options, playOptions);
+                return originalCreateSound(url, options);
             };
         }
     });
-};
+}
